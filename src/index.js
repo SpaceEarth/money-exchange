d1e06bd9cf22ea8e22de6809a5ecaffeec565e78
Aleksey <alexyasyuchenya@gmail.com>
2019-09-18 10:00:27 +0300

    Update index.js
    rewrite code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ const COINS = [50, 25, 10, 5, 1];
 const COINS_CHAR = ['H', 'Q', 'D', 'N', 'P'];
 
 module.exports = function makeExchange(currency) {
+    let i = 0,
+        money = {},
+        currentValue = currency;
+    
     if (currency > 10000) {
         return {error: "You are rich, my friend! We don't have so much coins for exchange"};
     }
     
-    let i = 0;
-    let money = {};
-    let currentValue = currency;
-
     while (currentValue > 0 && i < COINS.length) {
         if (currentValue >= COINS[i]) {
             money[COINS_CHAR[i]] = Math.trunc(currentValue / COINS[i]);
             currentValue = currentValue % COINS[i];
         }
-        i++;
+        i += 1;
     }
 
     return money;
